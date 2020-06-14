eef7ebd051bee349eed2be7106ab4c67187114e5
cattia <you@example.com>
2020-06-14 10:31:33 -0400

    [cricri] Ajout test Valides et invalides

--- src/demo/RegisterDemo.java
+++ src/demo/RegisterDemo.java
@@ -49,13 +49,13 @@ public class RegisterDemo
 		/* Create a list of items */
 		List<Item> grocery = new ArrayList<Item>();
 		grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 1.5));
-		grocery.add(new Item(Upc.generateCode("22804918500"), "Beef", 0.5, 5.75));
+		grocery.add(new Item(Upc.generateCode("22804918500"), "Beef", Integer.MAX_VALUE, 5.75));
 		grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", -1, 1.5));
 		grocery.add(new Item(Upc.generateCode("64748119599"), "Chewing gum", 2, 0.99));
 		grocery.add(new Item(Upc.generateCode("44348225996"), "Gobstoppers", 1, 0.99));
 		grocery.add(new Item(Upc.generateCode("34323432343"), "Nerds", 2, 1.44));
 		grocery.add(new Item(Upc.generateCode("54323432343"), "Doritos Club", 1, 0.5));
-		grocery.add(new Item(Upc.generateCode("54323433343"), "Doritos", 1, -1.25));
+		grocery.add(new Item(Upc.generateCode("54323433343"), "Doritos", 1, 1.25));
 
 
 		// Print receipt
--- src/registerTest/registerTest.java
+++ src/registerTest/registerTest.java
@@ -19,12 +19,52 @@ import stev.kwikemart.InvalidUpcException.UpcTooLongException;
 import stev.kwikemart.InvalidUpcException.UpcTooShortException;
 import stev.kwikemart.PaperRollException.OutOfPaperException;
 import stev.kwikemart.Register.DuplicateItemException;
+import stev.kwikemart.Register.NoSuchItemException;
 import stev.kwikemart.RegisterException.EmptyGroceryListException;
 import stev.kwikemart.RegisterException.TooManyItemsException;
 
 class RegisterTest {
+	/**
+	 * Classe d'équivalence : Entrées/Sorties :
+	 * 
+	 * CUP V1 : 1,3,4,6,7,8,9,0
+	 *  V2 : Au départ 2 
+	 *  V3 : Au départ 5 
+	 *  V4 : 12 Caractères
+	 * 
+	 * I1: nbCaractères>=13 
+	 * I2 : nbCaractères <=11
+	 * 
+	 * Liste :
+	 *  V5 : NbItems entre 1 et 10
+	 * 
+	 * I3 Nombre items <=0 
+	 * I4 Nombre item >= 11
+	 * 
+	 * Quantité :
+	 *  V6 : >0 
+	 *  V7 : <0 
+	 *  V8 : Qt Fractionnaire 
+	 *  V9 : qt Entier
+	 *
+	 * I5 : Qt < 0 Première entrée 
+	 * I6 : Qt > 0 article déjà présent 
+	 * I7 : QT fractionnaire pour item normal
+	 * 
+	 * Prix : 
+	 * V10 : Prix entre 0 et 35
+	 * 
+	 * I8 : Prix <= 0 
+	 * I9 Prix >=36
+	 *
+	 * Rabais : 
+	 * V11 : 5 produits
+	 *
+	 */
 	Register register;
 	List<Item> grocery;
+	Item myItem;
+	Item myItem2;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -35,216 +75,151 @@ class RegisterTest {
 	void tearDown() throws Exception {
 	}
 
+	/**
+	 * Génère des items aléatoires
+	 * 
+	 * @param maxItem
+	 * @param minPrice
+	 * @param maxPrice
+	 */
 	public void GenerateItems(int maxItem, int minPrice, int maxPrice) {
 		Register register = Register.getRegister();
 		grocery = new ArrayList<Item>();
 
 		Random randomNum = new Random();
 
-		int nbItems = 0;
+		int Quantity = 0;
+		double dQT = 0;
 		int price = 0;
-		String aze = "";
+		int FirstChar = 0;
+		String CUP = "";
 		for (int j = 0; j < maxItem; j++) {
 
-			for (int i = 0; i < 11; i++) {
-				nbItems = randomNum.nextInt(9 - 0) + 0;
-				price = randomNum.nextInt(maxPrice - minPrice) + minPrice;
-				if (nbItems == 5 || nbItems == 2) {
-					while (nbItems == 5 || nbItems == 2) {
-						nbItems = randomNum.nextInt(9 - 0) + 0;
-					}
-				}
+			for (int i = 0; i < 10; i++) {
 
-				aze = aze + nbItems;
+				Quantity = randomNum.nextInt(9 - 0) + 0;
+				FirstChar = randomNum.nextInt(9 - 0) + 0;
+				if (FirstChar == 5) {
+					FirstChar++;
+				}
+				if (FirstChar == 2) {
+					dQT = 0 + (9 - 0) * randomNum.nextDouble();
+				}
+				price = randomNum.nextInt(maxPrice - minPrice) + minPrice;
+
+				CUP = FirstChar + CUP + Quantity;
+			}
+			if (FirstChar == 2) {
+				myItem = new Item(Upc.generateCode(CUP), "Item " + j, Quantity, price);
+			} else {
+				myItem = new Item(Upc.generateCode(CUP), "Item " + j, dQT, price);
 			}
 
-			grocery.add(new Item(Upc.generateCode(aze), "Item " + j, nbItems, price));
-			aze = "";
+			if (grocery.contains(myItem)) {
+				j--;
+			} else {
+				grocery.add(myItem);
+			}
+
+			CUP = "";
 
 		}
 		System.out.println(register.print(grocery));
 	}
 
 	/**
-	 * VALIDES
+	 * Test des combinaisons suivantes : 
+	 * (V4 V1 V5 V6 V9 V10) Item 'Normal' dans la liste 
+	 * (V4 V2 V5 V6 V8 V10)  Item Fractionnaire dans la liste 
+	 * (V4 (V1 OU V2) V5 V7 V9 V10 ) Retrait article ( Quantité négatif ) 
+	 * (V4 V3 V5 V6 V9 V10 ) Ajout d'un coupon 
+	 * (V4 V1 V5 V6 V9 V10 V11) Item 'Normal' dans la liste pour avoir le rabais 
+	 * (V4 V3 V5 V6 V9 V10 ) Ajout d'un deuxieme coupon
 	 */
-	/**
-	 * Cas de test valide avec ue liste dont la taille est supérieure à 0 Des Items
-	 * dont le CUP est égal à 12
-	 * 
-	 * Dont le prix est inférieur à 35
-	 */
+
 	@Test
-	void testValidList() {
-		try {
-			register.changePaper(PaperRoll.SMALL_ROLL);
-			GenerateItems(1, 1, 12);
-			System.out.println(register.print(grocery));
-		} catch (OutOfPaperException e) {
-			register.changePaper(PaperRoll.LARGE_ROLL);
-			System.out.println(register.print(grocery));
-		}
+	public void testArticles() {
+		register.changePaper(PaperRoll.SMALL_ROLL);
+		grocery = new ArrayList<Item>();
+		grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 12));
+		grocery.add(new Item(Upc.generateCode("22345678901"), "Beef", 1.5, 13));
+		grocery.add(new Item(Upc.generateCode("82345678901"), "Mozzarella", 1, 12));
+		grocery.add(new Item(Upc.generateCode("82345678901"), "Mozzarella", -1, 12));
+		grocery.add(new Item(Upc.generateCode("82345678901"), "Mozzarella", 1, 12));
+		grocery.add(new Item(Upc.generateCode("52345678901"), "Coupon", 1, 1));
+		grocery.add(new Item(Upc.generateCode("32345678901"), "Table", 1, 20));
+		grocery.add(new Item(Upc.generateCode("52345668901"), "Coupon", 1, 1));
+		System.out.println(register.print(grocery));
 	}
 
 	/**
-	 * Ajout d'un item fractionnaire à la liste
-	 */
-	@Test
-	void testItemFrac() {
-		try {
-			register.changePaper(PaperRoll.SMALL_ROLL);
-			grocery = new ArrayList<Item>();
-			grocery.add(new Item(Upc.generateCode("25689784569"), "Beef ", 0.5, 2));
-			System.out.println(register.print(grocery));
-		} catch (OutOfPaperException e) {
-			register.changePaper(PaperRoll.LARGE_ROLL);
-			System.out.println(register.print(grocery));
-		}
-	}
-
-	/**
-	 * On retire un item de la liste
-	 */
-	@Test
-	void testItemQT() {
-		try {
-			register.changePaper(PaperRoll.SMALL_ROLL);
-			grocery = new ArrayList<Item>();
-			grocery.add(new Item(Upc.generateCode("72689784569"), "Arbre ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("72689784569"), "Arbre ", -1, 2));
-			grocery.add(new Item(Upc.generateCode("82689784569"), "Dortis ", 1, 2));
-			System.out.println(register.print(grocery));
-		} catch (OutOfPaperException e) {
-			register.changePaper(PaperRoll.LARGE_ROLL);
-			System.out.println(register.print(grocery));
-		}
-	}
-	
-	/**
-	 * Coupon 5 items Rabais
-	 */
-	@Test
-	void testFiveItemCoupon() {
-		try {
-			register.changePaper(PaperRoll.SMALL_ROLL);
-			grocery = new ArrayList<Item>();
-			grocery.add(new Item(Upc.generateCode("72689784569"), "Arbre ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("12689784569"), "Beef ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("82689784569"), "Dortis ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("22804918500"), "Beef", 0.5, 5.75));
-			grocery.add(new Item(Upc.generateCode("64748119599"), "Chewing gum", 2, 0.99));
-			grocery.add(new Item(Upc.generateCode("44348225996"), "Gobstoppers", 1, 0.99));
-
-			System.out.println(register.print(grocery));
-		} catch (OutOfPaperException e) {
-			register.changePaper(PaperRoll.LARGE_ROLL);
-			System.out.println(register.print(grocery));
-		}
-	}
-	/**
-	 * Coupon 
-	 */
-	@Test
-	void testitemCoupon() {
-		try {
-			register.changePaper(PaperRoll.SMALL_ROLL);
-			grocery = new ArrayList<Item>();
-			grocery.add(new Item(Upc.generateCode("72689784569"), "Arbre ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("12689784569"), "Beef ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("82689784569"), "Dortis ", 1, 2));
-			grocery.add(new Item(Upc.generateCode("22804918500"), "Beef", 0.5, 5.75));
-			grocery.add(new Item(Upc.generateCode("64748119599"), "Chewing gum", 2, 0.99));
-			grocery.add(new Item(Upc.generateCode("44348225996"), "Gobstoppers", 1, 0.99));
-			grocery.add(new Item(Upc.generateCode("54348225996"), "Coupon",1 , 1));
-			System.out.println(register.print(grocery));
-			
-		} catch (OutOfPaperException e) {
-			register.changePaper(PaperRoll.LARGE_ROLL);
-			System.out.println(register.print(grocery));
-			
-		}
-	}
-
-	/**
-	 * INVALIDES
-	 */
-	/**
-	 * Liste avec 0 'item
+	 * Test de la combinaison suivante : 
+	 * liste vide 
+	 * I3
 	 */
 	@Test
 	void testEmptyList() {
 		assertThrows(EmptyGroceryListException.class, () -> {
-			try {
-				register.changePaper(PaperRoll.SMALL_ROLL);
-				GenerateItems(0, 1, 12);
-				System.out.println(register.print(grocery));
-			} catch (OutOfPaperException e) {
-				register.changePaper(PaperRoll.LARGE_ROLL);
-				System.out.println(register.print(grocery));
-			}
+			register.changePaper(PaperRoll.LARGE_ROLL);
+			grocery = new ArrayList<Item>();
+			System.out.println(register.print(grocery));
+
 		});
 	}
 
 	/**
-	 * Liste avec trop d'item
+	 * Test de la combinaison suivante : 
+	 * V4 (V1 Ou V2) V6 (SI V1 -> V9 OU SI V2 ->V8)I4 
+	 * Liste avec trop d'items
+	 * 
 	 */
 	@Test
 	void testTooMuchItem() {
 		assertThrows(TooManyItemsException.class, () -> {
-			try {
-				register.changePaper(PaperRoll.SMALL_ROLL);
-				GenerateItems(12, 1, 12);
-				System.out.println(register.print(grocery));
-			} catch (OutOfPaperException e) {
-				register.changePaper(PaperRoll.LARGE_ROLL);
-				System.out.println(register.print(grocery));
-			}
+			register.changePaper(PaperRoll.LARGE_ROLL);
+			GenerateItems(12, 1, 12);
+			System.out.println(register.print(grocery));
 		});
 	}
 
 	/**
 	 * Liste avec item qui possède un CUP trop petit
+	 * I2 (V1 OU V2) V6 (SI V1 -> V9 OU SI V2 ->V8)
 	 */
 	@Test
 	void testInvalidCupTooSmall() {
 		assertThrows(UpcTooShortException.class, () -> {
-			try {
-				register.changePaper(PaperRoll.SMALL_ROLL);
-				grocery = new ArrayList<Item>();
-				grocery.add(new Item(Upc.generateCode(""), "Arbre ", 1, 2));
-				System.out.println(register.print(grocery));
-			} catch (OutOfPaperException e) {
 				register.changePaper(PaperRoll.LARGE_ROLL);
+				grocery = new ArrayList<Item>();
+				grocery.add(new Item(Upc.generateCode("123"), "Chaise ", 1, 2));
 				System.out.println(register.print(grocery));
-			}
 		});
 	}
 
 	/**
 	 * Liste avec item qui possède un CUP trop grand
+	 * I1 (V1 OU V2) V6 (SI V1 -> V9 OU SI V2 ->V8)
 	 */
 	@Test
 	void testInvalidCupTooBig() {
 		assertThrows(UpcTooLongException.class, () -> {
-			try {
-				register.changePaper(PaperRoll.SMALL_ROLL);
-				grocery = new ArrayList<Item>();
-				grocery.add(new Item(Upc.generateCode("722269784569"), "Arbre ", 1, 2));
-				System.out.println(register.print(grocery));
-			} catch (OutOfPaperException e) {
+			
 				register.changePaper(PaperRoll.LARGE_ROLL);
+				grocery = new ArrayList<Item>();
+				grocery.add(new Item(Upc.generateCode("722269784569"), "Cacahuete ", 1, 2));
 				System.out.println(register.print(grocery));
-			}
+		
 		});
 	}
 
 	/**
 	 * Item avec un prix négatif
+	 * I8 (V1 OU V2) V6 (SI V1 -> V9 OU SI V2 ->V8)
 	 */
 	@Test
 	void testAmountNegative() {
 		assertThrows(AmountException.class, () -> {
-			register.changePaper(PaperRoll.SMALL_ROLL);
+			register.changePaper(PaperRoll.LARGE_ROLL);
 			/* Create a list of items */
 			grocery = new ArrayList<Item>();
 			grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 1.5));
@@ -257,11 +232,13 @@ class RegisterTest {
 
 	/**
 	 * Item trop cher
+	 *  I9 (V1 OU V2) V6 (SI V1 -> V9 OU SI V2 ->V8) V4 
+	 *  
 	 */
 	@Test
 	void testAmountToExpensive() {
 		assertThrows(AmountException.class, () -> {
-			register.changePaper(PaperRoll.SMALL_ROLL);
+			register.changePaper(PaperRoll.LARGE_ROLL);
 			/* Create a list of items */
 			grocery = new ArrayList<Item>();
 			grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 1.5));
@@ -274,21 +251,50 @@ class RegisterTest {
 
 	/**
 	 * item fractionnaire avec mauvais CUP
+	 * I7 V2 V4 V6 V8
 	 */
 	@Test
 	void testWrongCupFracItem() {
 		assertThrows(InvalidQuantityForCategoryException.class, () -> {
-			try {
-				register.changePaper(PaperRoll.SMALL_ROLL);
-				grocery = new ArrayList<Item>();
-				grocery.add(new Item(Upc.generateCode("72269784569"), "Arbre ", 0.5, 2));
-				System.out.println(register.print(grocery));
-			} catch (OutOfPaperException e) {
+			
 				register.changePaper(PaperRoll.LARGE_ROLL);
+				grocery = new ArrayList<Item>();
+				grocery.add(new Item(Upc.generateCode("72269784569"), "Noix ", 0.5, 2));
 				System.out.println(register.print(grocery));
-			}
+			
 		});
 	}
-
+	/**
+	 * Premier Item avec quantité négative
+	 * I5 (V1 OU V2) V6 (SI V1 -> V9 OU SI V2 ->V8) V4
+	 */
+	@Test
+	void testQtNegFirstItem() {
+		assertThrows(NoSuchItemException.class, () -> {
+			
+				register.changePaper(PaperRoll.LARGE_ROLL);
+				grocery = new ArrayList<Item>();
+				grocery.add(new Item(Upc.generateCode("72269784569"), "Noix ", -5, 2));
+				System.out.println(register.print(grocery));
+			
+		});
+	}
+	/**
+	 * Dupliquer item dans la liste
+	 * I6 (V1 OU V2) V6 (SI V1 -> V9 OU SI V2 ->V8) V4
+	 */
+	@Test
+	void testQtItem() {
+		assertThrows(DuplicateItemException.class, () -> {
+			
+				register.changePaper(PaperRoll.LARGE_ROLL);
+				grocery = new ArrayList<Item>();
+				grocery.add(new Item(Upc.generateCode("72269784569"), "Noix ", 5, 2));
+				grocery.add(new Item(Upc.generateCode("72269784569"), "Noix ", 5, 2));
+				
+				System.out.println(register.print(grocery));
+			
+		});
+	}
 
 }
